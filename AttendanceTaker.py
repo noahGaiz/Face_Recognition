f165778a3669d405fce43be5f5c36903afb58dea
Nojus Gaizauskas <nojusgaizauskas@gmail.com>
2025-07-23 23:44:34 +0100

    Added more updates to project

---
+++ AttendanceTaker.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy
+import numpy as np
+import face_recognition
+from face_recognition import face_locations
+import os
+
+path = 'Image'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+
+for cls in myList:
+    curImg = cv2.imread(f'{path}/{cls}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cls)[0])
+print(classNames)
+
+def findEncodings(images):
+    encode_list = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encode_list.append(encode)
+
+    return encode_list
+
+encodeListsKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success , img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    faceCurrFrame = face_recognition.face_locations(imgS)
+    encodeCurrFrame = face_recognition.face_encodings(imgS,faceCurrFrame)
+
+    for encodeFace,faceLoc in zip(encodeCurrFrame,faceCurrFrame):
+        matches = face_recognition.compare_faces(encodeListsKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListsKnown,encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+    cv2.imshow('Webcame',img)
+    cv2.waitKey(1)
+
+
+
+# faceLoc = face_recognition.face_locations(imgMJ)[0]
+# encodeMJ = face_recognition.face_encodings(imgMJ)[0]
+# cv2.rectangle(imgMJ,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+#
+# faceLocTest = face_recognition.face_locations(imgTest)[0]
+# encodeTest = face_recognition.face_encodings(imgTest)[0]
+# cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+#
+# results = face_recognition.compare_faces([encodeMJ],encodeTest)
+# faceDist = face_recognition.face_distance([encodeMJ],encodeTest)
+
+
+
+
